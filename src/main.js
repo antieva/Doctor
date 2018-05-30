f82981957ad826189c972578164b6343cb46ab67
Dennise Ortega and Eva Antipina <dennise@>
2018-05-30 16:53:49 -0700

    add ui and html

--- src/main.js
+++ src/main.js
@@ -2,15 +2,40 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { template } from './template.js';
+import { bike } from './bike.js';
 
 $(document).ready(function() {
-  $('#starter-form').submit(function(event) {
+  $('#bike').submit(function(event) {
     event.preventDefault();
-    var goal = $('#goal').val();
-    var output = template(goal);
-    output.forEach(function(element) {
-      $('#solution').append("<li>" + element + "</li>");
-    });
+    let date = $('#date').val();
+    let location = $('#location').val();
+    $('#location').val("");
+    $('#date').val("");
+
+    let request = new XMLHttpRequest();
+    let url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${location}&distance=10&stolenness=proximity`;
+console.log(url);
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        let response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+    let getElements = function(response) {
+      console.log(response);
+      $('.showOutput').append('<h4>' + `The brand of the bike is ${response.bikes[0].manufacturer_name}.` + '</h4>');
+      $('.showOutput').append('<p>' + `The serial number is ${response.bikes[0].serial}.` + '</p>');
+      $('.showOutput').append('<p>' + `The color of the bike is ${response.bikes[0].frame_colors}.` + '</p>');
+      $('.showOutput').append('<p>' + `Date stolen: ${response.bikes[0].date_stolen}.` + '</p>');
+      }
+
+    //   function(error) {
+    //   $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    // }
+
   });
 });
