46d558b1eccab4edaae8bcc1d95ce2c4498dc026
Eva Antipina <eva.antipina@gmail.com>
2018-06-01 17:47:38 -0700

    Clean up the code.

--- src/handleValue.js
+++ src/handleValue.js
@@ -25,7 +25,6 @@ export function handle(value) {
    let counter = 0;
    response = JSON.parse(response);
    let result = response.data;
-   console.log(result);
    $('#output').hide();
    $('.showOutput').empty();
    $('.showHowMany').empty();
@@ -35,7 +34,6 @@ export function handle(value) {
      let practicesCounter = 0;
      let practices = doctor.practices;
      practices.forEach(function(practice) {
-       console.log(practice.within_search_area);
        if (practice.within_search_area) {
          practicesCounter++;
        }
@@ -46,7 +44,6 @@ export function handle(value) {
        $('.showOutput').append(`Locations: <br><br>`);
 
        practices.forEach(function(practice) {
-         console.log(practice.name);
          if (practice.within_search_area) {
            $('.showOutput').append(`<h6>${practice.name}</h6>`);
            $('.showOutput').append(`Website: <a href ='${practice.website}'>${practice.website || 'N/A'}</a><br>`);
